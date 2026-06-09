84b7caf1e7df7b856ca9d914ff4c937df19678b3
Ehab <ehab.ahmed5174@gmail.com>
2026-06-09 19:23:22 +0300

    I built APIs for booking, and menuitems

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItems(models.Model):
+    title= models.CharField(max_length=255)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
+    inventory= models.SmallIntegerField()
+    def __str__(self):
+        return self.title
+
 class Booking(models.Model):
     name= models.CharField(max_length=255)
     no_of_guests= models.IntegerField()
     bookingDate= models.DateField()
-
-class Menu(models.Model):
-    title= models.CharField(max_length=255)
-    price= models.DecimalField(max_digits=10, decimal_places=2)
-    inventory= models.SmallIntegerField()
+    # def __str__(self):
+    #     return self.title
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from.models import MenuItems, Booking
+from.serializers import MenuItemSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset= MenuItems.objects.all()
+    serializer_class= MenuItemSerializer
+
+class SingleMenuItemsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= MenuItems.objects.all()
+    serializer_class= MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset= Booking.objects.all()
+    serializer_class= BookingSerializer
+    permission_classes= [permissions.IsAuthenticated]
